f08cf200cc2620d551411cb03fe32f6ba8021822
JP Navarro <jpfnavarro@gmail.com>
2019-12-18 21:39:49 +0000

    Rename router

--- sbin/route_xdcdb.sh
+++ sbin/route_xdcdb.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-APP_BASE=/soft/warehouse-apps-1.0/Manage-XDCDB
-APP_SOURCE=${APP_BASE}/PROD
-WAREHOUSE_SOURCE=/soft/warehouse-1.0/PROD
+MY_ROOT=/soft/warehouse-apps-1.0/Manage-XDCDB/PROD
+WAREHOUSE_ROOT=/soft/warehouse-1.0/PROD
+PYTHON=/soft/python-current/bin/python
+export LD_LIBRARY_PATH=/soft/python-current/lib/
 
-PYTHON_BASE=${APP_BASE}/`cat python/lib/python*/orig-prefix.txt`
-export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
-
-PIPENV_BASE=${APP_BASE}/python
-source ${PIPENV_BASE}/bin/activate
-
-PYTHON_BIN=python3
-
-export PYTHONPATH=${WAREHOUSE_SOURCE}/django_xsede_warehouse
-export DJANGO_CONF=${APP_BASE}/conf/django_xsede_warehouse.conf
+export PYTHONPATH=${WAREHOUSE_ROOT}/django_xsede_warehouse
+export DJANGO_CONF=/soft/warehouse-1.0/conf/settings_info_mgmt.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
 
-${PYTHON_BIN} ${APP_SOURCE}/sbin/route_xdcdb.py ${@:1}
+${PYTHON} ${MY_ROOT}/sbin/route_xdcdb.py ${@:1}
