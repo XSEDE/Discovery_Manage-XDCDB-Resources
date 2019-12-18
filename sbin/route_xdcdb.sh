4e1c55662f0f5340da65ab377461397f355e2594
JP Navarro <jpfnavarro@gmail.com>
2019-12-18 21:39:49 +0000

    Rename router

--- sbin/route_xdcdb.sh
+++ sbin/route_xdcdb.sh
@@ -9,10 +9,8 @@ export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
 PIPENV_BASE=${APP_BASE}/python
 source ${PIPENV_BASE}/bin/activate
 
-PYTHON_BIN=python3
-
 export PYTHONPATH=${WAREHOUSE_SOURCE}/django_xsede_warehouse
 export DJANGO_CONF=${APP_BASE}/conf/django_xsede_warehouse.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
 
-${PYTHON_BIN} ${APP_SOURCE}/sbin/route_xdcdb.py ${@:1}
+${PIPENV_BASE}/bin/python ${APP_SOURCE}/sbin/route_xdcdb.py ${@:1}
