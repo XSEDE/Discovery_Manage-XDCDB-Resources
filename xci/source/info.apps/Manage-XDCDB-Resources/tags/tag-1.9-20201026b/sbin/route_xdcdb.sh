751c7d4e685fe517c7d8ec29f3d3ead72f375eba
navarro <navarro@1370f987-b873-4e1f-8c66-8b3e5403fd71>
2020-10-26 17:49:25 +0000

    For production

---
+++ xci/source/info.apps/Manage-XDCDB-Resources/tags/tag-1.9-20201026b/sbin/route_xdcdb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+APP_BASE=/soft/warehouse-apps-1.0/Manage-XDCDB-Resources
+APP_SOURCE=${APP_BASE}/PROD
+WAREHOUSE_SOURCE=/soft/warehouse-1.0/PROD
+
+PYTHON_BASE=${APP_BASE}/`cat python/lib/python*/orig-prefix.txt`
+export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
+
+PIPENV_BASE=${APP_BASE}/python
+source ${PIPENV_BASE}/bin/activate
+
+PYTHON_BIN=python3
+
+export PYTHONPATH=${WAREHOUSE_SOURCE}/django_xsede_warehouse
+export DJANGO_CONF=${APP_BASE}/conf/django_xsede_warehouse.conf
+export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
+
+${PYTHON_BIN} ${APP_SOURCE}/sbin/route_xdcdb.py ${@:1}
